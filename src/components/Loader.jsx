111279900cb1fadcaa2c6238d0c493ce7278059c
StanislavKuchma <kuchmastanislav73@gmail.com>
2022-07-18 23:11:48 +0300

    fix: loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,31 +1,9 @@
-import { useState, CSSProperties } from 'react';
 import ClipLoader from 'react-spinners/ClipLoader';
 
 export const Loader = () => {
-  let [loading, setLoading] = useState(true);
-  let [color, setColor] = useState('#ffffff');
-
-  const CSSProperties = {
-    display: 'block',
-    margin: '0 auto',
-    borderColor: 'red',
-  };
-
   return (
     <div className="sweet-loading">
-      {/* <button onClick={() => setLoading(!loading)}>Toggle Loader</button>
-      <input
-        value={color}
-        onChange={input => setColor(input.target.value)}
-        placeholder="Color of the loader"
-      /> */}
-
-      <ClipLoader
-        color={color}
-        loading={loading}
-        // cssOverride={override}
-        size={24}
-      />
+      <ClipLoader size={20} />
     </div>
   );
 };
