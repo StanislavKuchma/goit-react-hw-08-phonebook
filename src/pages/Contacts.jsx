002e5ac20301b462271fcded6e4643c4222b18d3
StanislavKuchma <kuchmastanislav73@gmail.com>
2022-08-01 19:58:06 +0300

    fix: hover focus

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -4,12 +4,16 @@ import { Filter } from '../components/Filter';
 
 export default function Contacts() {
   return (
-    <>
+    <div
+      style={{
+        backgroundColor: '#fff',
+      }}
+    >
       <h1>Phonebook</h1>
       <Form />
       <Filter />
       <h2>Contact</h2>
       <ContactList />
-    </>
+    </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
       }}
     >
       <Navigation />
-      <Outlet style={{ backgroundColor: 'none' }} />
+      <Outlet />
     </div>
   );
 };
